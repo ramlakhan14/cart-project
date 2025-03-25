7c70e15e3ed833f3f08e5e191bde1c2a27ec58b6
ramlakhan14 <ramlakhan149511@gmail.com>
2025-03-26 00:55:18 +0530

    Updated changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
-<h1>Hello Ramm</h1>
+      <CartItem />
     </div>
   );
 }
